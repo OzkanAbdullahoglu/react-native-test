e232f5bbb3e4a008474450d7f7030e8dc8e39401
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2019-01-01 03:05:55 +0300

    Route paths are defined in tabnavigator and It's fixed at the bottom

--- TabNavigator.js
+++ TabNavigator.js
@@ -1,90 +1,174 @@
 import React from "react";
-import {
-  Ionicons,
-  MaterialIcons,
-  MaterialCommunityIcons
-} from "@expo/vector-icons";
-import { Platform } from "react-native";
-import { StyleSheet, Text, View } from "react-native";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import CollectData from "./CollectData";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { ActivityIndicator, Platform } from "react-native";
+import { AsyncStorage, StyleSheet, View } from "react-native";
 import Meteor from "./screens/Meteor";
 import ReactScreen from "./screens/ReactScreen";
 import Javascript from "./screens/Javascript";
 import OtherLang from "./screens/OtherLang";
 import Settings from "./screens/Settings";
-import { List, ListItem } from "react-native-elements";
-import { NativeRouter, Route, Link } from "react-router-native";
-
-const client = new ApolloClient({
-  uri: "http://fiipractic-react.cultofcoders.com/graphql"
-});
+import { Redirect, NativeRouter, Route, Link } from "react-router-native";
 
 export default class TabNavigator extends React.Component {
+  state = {
+    initRouteHandler: "",
+    isLoading: true
+  };
+
+  componentDidMount() {
+    this.initRouteManage();
+  }
+
+  setInitRoute = async initRoute => {
+    try {
+      await AsyncStorage.setItem("initRoute", initRoute);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  getInitRoute = async () => {
+    let initialRoute = "";
+    try {
+      initialRoute = await AsyncStorage.getItem("initRoute");
+      if (initialRoute !== "") {
+        this.setState({ isLoading: false });
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+    return initialRoute;
+  };
+
+  initRouteManage = () => {
+    this.getInitRoute().then(res => {
+      this.setState({ initRouteHandler: res });
+    });
+  };
+
   render() {
     return (
-      <NativeRouter>
-        <View style={{ flex: 1 }}>
-          <List>
-            <ApolloProvider client={client}>
-              <CollectData />
-            </ApolloProvider>
-          </List>
-          <View style={styles.container}>
-            <Link to="/screens/javascript">
-              <MaterialCommunityIcons
-                name="language-javascript"
-                size={32}
-                color="blue"
-              />
-            </Link>
-            <Link to="/screens/meteor">
-              <MaterialCommunityIcons name="meteor" size={32} color="orange" />
-            </Link>
-            <Link to="/screens/ReactScreen">
-              <MaterialCommunityIcons name="react" size={32} color="blue" />
-            </Link>
-            <Link to="/screens/other">
-              <Ionicons
-                name={
-                  Platform.OS === "ios" ? "ios-code-working" : "md-code-working"
-                }
-                size={32}
-                color="purple"
-              />
-            </Link>
-            <Link to="/screens/settings">
-              <Ionicons
-                name={Platform.OS === "ios" ? "ios-settings" : "md-settings"}
-                size={32}
-                color="lightgray"
-              />
-            </Link>
+      <View style={{ flex: 1 }}>
+        {this.state.isLoading ? (
+          <View
+            style={{
+              paddingVertical: 20,
+              borderTopWidth: 1,
+              borderBottomWidth: 0,
+              borderColor: "#CED0CE"
+            }}
+          >
+            <ActivityIndicator animating="true" size="large" color="#0000ff" />
           </View>
-          <Route path="/screens/javascript" component={Javascript} />
-          <Route path="/screens/meteor" component={Meteor} />
-          <Route path="/screens/reactscreen" component={ReactScreen} />
-          <Route path="/screens/other" component={OtherLang} />
-          <Route path="/screens/settings" component={Settings} />
-        </View>
-      </NativeRouter>
+        ) : (
+          <NativeRouter>
+            <View style={{ flex: 1 }}>
+              <Route
+                path="/javascript"
+                component={() => (
+                  <Javascript initialRoute={this.setInitRoute} />
+                )}
+              />
+              <Route
+                path="/meteor"
+                component={() => <Meteor initialRoute={this.setInitRoute} />}
+              />
+              <Route
+                path="/reactscreen"
+                component={() => (
+                  <ReactScreen initialRoute={this.setInitRoute} />
+                )}
+              />
+              <Route
+                path="/other"
+                component={() => <OtherLang initialRoute={this.setInitRoute} />}
+              />
+              <Route
+                path="/settings"
+                component={() => <Settings initialRoute={this.setInitRoute} />}
+              />
+              <View style={styles.container}>
+                {this.state.initRouteHandler === "javascript" ? (
+                  <View>
+                    <Redirect to="/javascript" />
+                  </View>
+                ) : this.state.initRouteHandler === "meteor" ? (
+                  <View>
+                    <Redirect to="/meteor" />
+                  </View>
+                ) : this.state.initRouteHandler === "reactscreen" ? (
+                  <View>
+                    <Redirect to="/reactscreen" />
+                  </View>
+                ) : this.state.initRouteHandler === "other" ? (
+                  <View>
+                    <Redirect to="/other" />
+                  </View>
+                ) : (
+                  <View>
+                    <Redirect to="/settings" />
+                  </View>
+                )}
+                <Link to="/javascript">
+                  <MaterialCommunityIcons
+                    name="language-javascript"
+                    size={60}
+                    color="#f0db4f"
+                  />
+                </Link>
+                <Link to="/meteor">
+                  <MaterialCommunityIcons
+                    name="meteor"
+                    size={60}
+                    color="#de4f4f"
+                  />
+                </Link>
+                <Link to="/reactscreen">
+                  <MaterialCommunityIcons
+                    name="react"
+                    size={60}
+                    color="#26d9fd"
+                  />
+                </Link>
+                <Link to="/other">
+                  <Ionicons
+                    name={
+                      Platform.OS === "ios"
+                        ? "ios-code-working"
+                        : "md-code-working"
+                    }
+                    size={60}
+                    color="purple"
+                  />
+                </Link>
+                <Link to="/settings">
+                  <Ionicons
+                    name={
+                      Platform.OS === "ios" ? "ios-settings" : "md-settings"
+                    }
+                    size={60}
+                    color="#7e7a7a"
+                  />
+                </Link>
+              </View>
+            </View>
+          </NativeRouter>
+        )}
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    width: 370,
-    height: 50,
-    left: 5,
-    position: "absolute",
-    top: 500,
+    flex: 1,
     bottom: 0,
-    backgroundColor: "#f8f8f8",
+    height: 30,
+    paddingBottom: 20,
+    flexDirection: "row",
+    backgroundColor: "#fff7ee",
     justifyContent: "space-around",
-    alignItems: "center",
+    alignItems: "flex-end",
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2
