2b8b4d2678e16d0f83fbe7ad1f3c8154070d1bde
OzkanAbdullahoglu <ozkanabdullahoglu@gmail.com>
2019-01-01 03:13:42 +0300

    a component with handles comment mutation

---
+++ CommentMutation.js
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Mutation, graphql, compose } from "react-apollo";
+import { Modal, View, TextInput } from "react-native";
+import { Button } from "react-native-elements";
+
+const ADD_COMMENT = gql`
+  mutation addComment($input: CommentCreateInput) {
+    addComment(input: $input) {
+      text
+      userId
+    }
+  }
+`;
+
+const Query = gql`
+  {
+    users {
+      _id
+      firstname
+      lastname
+      email
+    }
+  }
+`;
+
+const AddComment = props => {
+  return (
+    <Mutation
+      mutation={ADD_COMMENT}
+      refetchQueries={() => {
+        const queryPosts = gql`
+          query getPosts($filters: JSON, $options: JSON) {
+            posts(filters: $filters, options: $options) {
+              _id
+              categoryId
+              userId
+              title
+              description
+              createdAt
+              user {
+                _id
+                firstname
+                lastname
+                email
+              }
+              category {
+                _id
+                name
+              }
+              tags {
+                _id
+                name
+              }
+              comments {
+                user {
+                  firstname
+                  lastname
+                  email
+                }
+                text
+                createdAt
+              }
+            }
+          }
+        `;
+        return [
+          {
+            query: queryPosts,
+            variables: {
+              filters: props.filter
+            }
+          }
+        ];
+      }}
+    >
+      {(addComment, { data }) => (
+        <View>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={props.visible}
+          >
+            <View
+              style={{
+                flex: 1,
+                paddingTop: 220,
+                backgroundColor: "rgba(52, 52, 52, 0.8)"
+              }}
+            >
+              <TextInput
+                style={{
+                  height: 150,
+                  borderColor: "white",
+                  borderWidth: 1,
+                  backgroundColor: "white",
+                  margin: 15,
+                  padding: 10
+                }}
+                placeholder="Your Comment"
+                onChangeText={text => props.updateText(text)}
+              />
+              <Button
+                onPress={() => {
+                  addComment({
+                    variables: {
+                      input: {
+                        text: props.singleComment,
+                        postId: props.postId,
+                        userId:
+                          props.data.users[Math.floor(Math.random() * 10)]._id
+                      }
+                    }
+                  });
+                  props.visible();
+                }}
+                MaterialCommunityIcons={{ name: "comment-outline" }}
+                backgroundColor="#03A9F4"
+                buttonStyle={{
+                  borderRadius: 0,
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginBottom: 0
+                }}
+                title="Send"
+              />
+            </View>
+          </Modal>
+        </View>
+      )}
+    </Mutation>
+  );
+};
+
+export default compose(graphql(Query))(AddComment);
